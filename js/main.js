a395647791e6a3abdc8dab79a23670697f5bd506
alisher <alisher.tojimov@yandex.ru>
2022-03-31 16:23:57 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,82 @@
+//string
+
+let firstName = "Chris";
+console.log(firstName);
+let secondName = "Mike";
+console.log(secondName);
+let third_name = "David";
+console.log(third_name);
+
+//number
+
+let firstAge = "18";
+console.log(firstAge);
+let secondAge = "22";
+console.log(secondAge);
+let third_age = "19";
+console.log(third_age);
+
+//BigInt
+
+let bigNum1 = "9545959445551514848";
+console.log(bigNum1);
+let bigNum2 = "3385118431384131384";
+console.log(bigNum2);
+let bigNum3 = "5615451215181512123";
+console.log(bigNum3);
+
+//Boolean
+
+let isMarried = false;
+console.log(isMarried);
+let is_student = true;
+console.log(is_student);
+
+//undefined
+
+let x;
+console.log(x);
+const y = undefined;
+console.log(y);
+
+//null
+
+let z = null;
+console.log(z);
+const r = null;
+console.log(z);
+
+//symbol
+
+let from = Symbol("Uzbekistan");
+console.log(from);
+var userName = Symbol("Salim");
+console.log(userName);
+const userAge = Symbol ("18");
+console.log(userAge);
+
+//Object
+
+let student = {
+    studentName : 'Karim',
+    studentAge : 24,
+    student_university : 'Tashkent University of  Information Technology'
+};
+console.log(student);
+
+var developer = {
+    devName : "Akmal",
+    devAge : 22,
+    dev_direction: "frontend"
+}
+console.log(developer);
+
+const footballer = {
+    footballerName : "Benzema",
+    footballerAge : "34",
+    footballer_position : "Forward"
+}
+console.log(footballer);
+
+
+
